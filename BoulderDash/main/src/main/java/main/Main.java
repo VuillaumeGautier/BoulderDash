afb926c6c80f119926324b2b8d4eb6fcbeade18b
gauti <gauti@LAPTOP-AJGGPIV3>
2017-06-13 12:59:10 +0200

    test2

--- BoulderDash/main/src/main/java/main/Main.java
+++ BoulderDash/main/src/main/java/main/Main.java
@@ -22,7 +22,7 @@ public abstract class Main {
      */
     public static void main(final String[] args) {
         final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
-        //hugo
+        //hugo;gautier
         try {
             controller.start();
         } catch (final SQLException exception) {
