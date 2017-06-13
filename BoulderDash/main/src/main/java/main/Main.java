117eb26f9755dd2cd40be9b0840774f93885fc6e
Hugo-r20 <hugo.routy@viacesi.fr>
2017-06-13 13:01:39 +0200

    fin test

--- BoulderDash/main/src/main/java/main/Main.java
+++ BoulderDash/main/src/main/java/main/Main.java
@@ -22,7 +22,6 @@ public abstract class Main {
      */
     public static void main(final String[] args) {
         final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
-        //hugo;gautier;Manu Julien mdr XD ptdr
         try {
             controller.start();
         } catch (final SQLException exception) {
