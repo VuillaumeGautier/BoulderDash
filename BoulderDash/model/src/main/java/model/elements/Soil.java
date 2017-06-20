2a48c59d087dca619d15a07a486945434b1e6f2f
gauti <gauti@LAPTOP-AJGGPIV3>
2017-06-20 10:41:50 +0200

    javadoc of soil missing

--- BoulderDash/model/src/main/java/model/elements/Soil.java
+++ BoulderDash/model/src/main/java/model/elements/Soil.java
@@ -1,5 +1,12 @@
 package model.elements;
 
+/**
+ * <h1>The Class MetadataDAO.</h1>
+ * 
+ * @author Vuillaume Gautier
+ * @version 1.0
+ */
+
 public class Soil extends Ground {
 
 	private static String SPRITE = "soil.png";
