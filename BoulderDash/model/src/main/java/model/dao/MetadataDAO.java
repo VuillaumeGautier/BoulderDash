342486ec99d3fb0c69c9f1e2c4e0c8ac5211bcff
Hugo-r20 <hugo.routy@viacesi.fr>
2017-06-21 09:17:55 +0200

    test dao

--- BoulderDash/model/src/main/java/model/dao/MetadataDAO.java
+++ BoulderDash/model/src/main/java/model/dao/MetadataDAO.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
  * @author Hugo Routy
  * @version 1.0
  */
-public class MetadataDAO extends AbstractDAO {
+public abstract class MetadataDAO extends AbstractDAO {
 
 	/**
 	 * Gets the height of the map
