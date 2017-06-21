5060c9e972f364b42a0aedddb1dbae9685a87c64
Emmanuel <Emmanuel@Emmanuel-PC.mshome.net>
2017-06-21 10:26:11 +0200

    modif control

--- BoulderDash/controller/src/main/java/controller/BoulderDashController.java
+++ BoulderDash/controller/src/main/java/controller/BoulderDashController.java
@@ -1,17 +1,58 @@
 package controller;
 
-import model.*;
-import view.*;
+import model.* ;
+import view.BoulderDashView; 
+
 
 public class BoulderDashController implements contract.IController {
 
 	private BoulderDashModel model;
 	private BoulderDashView view;
 	public static int frameRate = 500;
+	
 
 	public void play() {
 		// TODO - implement BoulderDashController.play
-		
+while (model.getMap().getLevelEnded() == false){
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			for(int y = model.getMap().getHeight() - 1 ; y == 0 ; y--){
+				for(int x = 0; x == model.getMap().getWidth() - 1  ; x++){
+					if (model.getMap().getOnTheMapXY(x, y).getSprite() == "player.png"){
+						model.getMap().getOnTheMapXY(x, y).move(x, y, view.KeyUser() );
+					}
+					else {
+						model.getMap().getOnTheMapXY(x, y).move(x, y ,0);
+					}
+					if (model.getMap().getDiamondsNeeded() == model.getMap().getDiamondsPicked() ){
+						if (model.getMap().getOnTheMapXY( model.getMap().getDoorX(),model.getMap().getDoorY()).getSprite() =="player.png")  {
+							model.getMap().setLevelEnded(true );
+						}
+						model.getMap().setOnTheMapXY(new model.elements.Door(), model.getMap().getDoorX(),model.getMap().getDoorX());
+						
+						
+					}
+				}
+				
+			}
+			for(int y = 0 ; y == model.getMap().getHeight() - 1 ; y++){
+				for(int x = 0; x == model.getMap().getWidth() - 1  ; x++){
+					view.show(x, y, model.getMap().getOnTheMapXY(x, y).getSprite());
+					
+				}
+			}
+				
+		}
+		if (model.getMap().getLevelLost() == false ){
+			view.displaySuccess();
+		}
+		else{
+			view.displayGameOver();
+		}
 	}
 
 	public BoulderDashModel getModel() {
