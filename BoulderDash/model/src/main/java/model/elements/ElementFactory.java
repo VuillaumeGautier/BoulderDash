0eec12c87337038c3039982be0d0022c768d8a8d
julie <julie@10.154.128.79>
2017-06-22 08:32:20 +0200

    Factory

--- BoulderDash/model/src/main/java/model/elements/ElementFactory.java
+++ BoulderDash/model/src/main/java/model/elements/ElementFactory.java
@@ -17,7 +17,7 @@ public abstract class ElementFactory {
 	private static Enemy Enemy = new Enemy();
 	private static Player Player = new Player();
 	private static Void Void = new Void();
-	private static Soil Soil = new Soil();
+	private static Soil Soil = new Soil(); 
 	private static Door Door = new Door();
 	private static Border Border = new Border();
 	private static DestructibleWall Destructible = new DestructibleWall();
